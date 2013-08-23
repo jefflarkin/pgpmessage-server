b2a9e2d79a168833840c43f121ba87e54d1225ef
jefflarkin <jefflarkin@gmail.com>
2013-08-23 19:57:42 -0400

    Added KeyId field, misc clean-ups

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,13 @@
 $(function () {
     var pubkeyText = $("#pubkey").text();
     if (window.crypto.getRandomValues) {
+        var openpgp = window.openpgp;
         openpgp.init();
         var pubkey = openpgp.read_publicKey(pubkeyText);
         var encode = function () {
             var message = "From: " + $("#from").val() + "\n";
             message += "Reply To: " + $("#reply").val() + "\n";
+            message += "KeyID: " + $("#keyid").val() + "\n";
             message += "Subject: " + $("#subject").val() + "\n";
             message += $("#cleartext").val();
             var cyphertext = openpgp.write_encrypted_message(pubkey, message);
@@ -13,10 +15,10 @@ $(function () {
             $("#results").show();
             return cyphertext;
         };
-        $(["#from","#reply","#subject","#cleartext"]).each(function(idx,el) 
+        /*$(["#from","#reply","#subject","#cleartext"]).each(function(idx,el) 
         {
             $(el).on("keyup",encode);
-        });
+        });*/
         $("#submit").on("click", function(ev)
         {
             ev.preventDefault();
@@ -27,18 +29,17 @@ $(function () {
                 // Verify returned data
                 if (data == encoded) 
                 {
-                    console.log([data,encoded]);
                     $("#status").html("Message sent and verified.").removeClass("alert-info").addClass("alert-success").show();
                 } else 
                 {
-                    $("#status").html("Message sent, but verification failed.").removeClass("alert-info").addClass("alert-error").show();
+                    $("#status").html("Message sent, but verification failed.").removeClass("alert-info").addClass("alert-danger").show();
                 }
                 // Give option to view sent message
                 // Clear form
                 $("#form").find("input[type=text], textarea").val("");
             }).fail(function()
             {
-                $("#status").html("Failed to send message. Please refresh the page and try again.").removeClass("alert-info").addClass("alert-error").show();
+                $("#status").html("Failed to send message. Please refresh the page and try again.").removeClass("alert-info").addClass("alert-danger").show();
             });
         });
     }
